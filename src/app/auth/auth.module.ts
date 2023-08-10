28dac6ac6945d27c421ef36063556ad6f68239b4
tilakved <tilakved3898@gmail.com>
2023-08-10 17:44:42 +0530

    Added Login Page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, RouterModule],
   exports: [SignupComponent],
 })
 export class AuthModule {}

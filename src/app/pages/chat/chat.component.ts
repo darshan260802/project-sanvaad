501cb9c20571acd489efc0b4a5fe9e002000e6ec
Darshan Patel <darshan.patel@weboccult.com>
2023-08-17 09:38:11 +0530

    Added User Info in firestore

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -6,15 +6,19 @@ import { HelperService } from 'src/app/core/services/helper.service';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
   showSettingsDropdown: boolean = false;
-  user:any = null;
+  user: any = null;
+  searchQuery = '';
+  searchResult: any[] = [];
+  searchTimer: any = null;
+  conversationsList: any[] = [];
   constructor(private helper: HelperService, private router: Router) {}
 
   async ngOnInit() {
-    this.user = await  this.helper.firebase.getUserInfo();
+    this.user = await this.helper.firebase.getUserInfo();
     this.toggleMode();
   }
 
@@ -33,36 +37,28 @@ export class ChatComponent implements OnInit {
     );
 
     document.documentElement.classList.toggle('dark');
-
-    //
-    // let themeToggleBtn: any = document.getElementById('theme-toggle');
-    //
-    // themeToggleBtn?.addEventListener('click', function () {
-    //
-    //   // toggle icons inside button
-    //   themeToggleDarkIcon?.classList.toggle('hidden');
-    //   themeToggleLightIcon?.classList.toggle('hidden');
-    //
-    //   // if set via local storage previously
-    //   if (localStorage.getItem('color-theme')) {
-    //     if (localStorage.getItem('color-theme') === 'light') {
-    //       document.documentElement.classList.add('dark');
-    //       localStorage.setItem('color-theme', 'dark');
-    //     } else {
-    //       document.documentElement.classList.remove('dark');
-    //       localStorage.setItem('color-theme', 'light');
-    //     }
-    //
-    //     // if NOT set via local storage previously
-    //   } else {
-    //     if (document.documentElement.classList.contains('dark')) {
-    //       document.documentElement.classList.remove('dark');
-    //       localStorage.setItem('color-theme', 'light');
-    //     } else {
-    //       document.documentElement.classList.add('dark');
-    //       localStorage.setItem('color-theme', 'dark');
-    //     }
-    //   }
-    // })
   }
+
+  handleSearch() {
+    clearTimeout(this.searchTimer);
+
+    this.searchTimer = setTimeout(() => {
+      this.helper.firebase.getSearchedUsers(this.searchQuery).then(res => {
+        console.log('SEARCH DATA', res);
+        this.searchResult = res;
+      }).catch(err => {
+        console.log('SEARCH ERROR', err);
+        
+      });
+    }, 1000);
+  }
+
+  createConversation(receiverId:string){
+    this.helper.firebase.createConversation(receiverId).then(res=>{
+      console.log('CONVERSATION', res);
+      
+    })
+  }
+
+  
 }
